541261687ff8fb8a9a09400c416e0d698d69258c
KaguyaIndustries <github@kyonbox.de>
2026-03-05 13:14:07 +0100

    added bot code lol

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token, channelid, originname } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ]
+});
+
+const TARGET_CHANNEL_ID = channelid;
+const FIXED_NAME = originname;
+
+client.once('ready', () => {
+    console.log(`Gatekeeper readyy`);
+});
+
+client.on('channelUpdate', async (oldChannel, newChannel) => {
+    if (newChannel.id !== TARGET_CHANNEL_ID) return;
+    if (newChannel.name !== FIXED_NAME) {
+        console.log(`Irgendso ein Paul hat den Channel zu "${newChannel.name}" geändert! Cringe..`);
+        try {
+            await newChannel.setName(FIXED_NAME);
+            console.log(`Yaaasss, Name ist wiedew safe "${FIXED_NAME}" UwU 💖`);
+        } catch (error) {
+            console.error(`Oooopsie fucky wucky, Discowd sagt nein (Ewwow):`, error);
+        }
+    }
+});
+
+client.login(token);
